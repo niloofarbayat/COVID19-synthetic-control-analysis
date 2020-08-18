4fd05a2ec4ec3a2bf79099df2cb49f9eac542ff5
Morrin0 <codymorrin@gmail.com>
2020-08-17 19:29:22 -0700

    updating apple mobility data now uses concurrent processes

--- synthetic_control_analysis/load_and_clean.py
+++ synthetic_control_analysis/load_and_clean.py
@@ -3,6 +3,9 @@ import sys
 import numpy as np
 import pandas as pd
 import datetime
+from subprocess import Popen
+
+import time
 
 
 
@@ -309,6 +312,35 @@ def _update_google():
 
 # update Apple data
 def _update_apple():
+    apple_hidden_path = "../data/mobility/.applemobilitytrends.csv"
+    try:
+        date_last_mod = datetime.date.fromtimestamp(os.path.getmtime(apple_hidden_path))
+    except:
+        date_last_mod = datetime.date.min
+
+    for day in range(7):
+        attempt_date = datetime.date.today() - datetime.timedelta(days=day)
+        if attempt_date < date_last_mod:
+            os.system(powershell_path + "cp %s %s" % (apple_hidden_path, _apple_local_path))
+            os.system(powershell_path + "touch %s" % apple_hidden_path)
+            return 0
+        # attempt to find url:
+        processes = []
+        for i in range(14, 14+10):
+            for dev in range(20):
+                processes.append(Popen("curl -s -f -o %s -z %s %s" % (apple_hidden_path, apple_hidden_path, _apple_web_path % (i, dev, attempt_date.strftime('%Y-%m-%d'))), shell=True))
+        if not all(p.wait() for p in processes):
+            os.system(powershell_path + "cp %s %s" % (apple_hidden_path, _apple_local_path))
+            os.system(powershell_path + "touch %s" % apple_hidden_path)
+            return 0
+
+    print("Unable to update Apple mobility data", file=sys.stderr)
+    return 1
+
+
+'''
+# OLD APPLE UPDATE CODE:
+def _update_apple():
     apple_hidden_path = "../data/mobility/.applemobilitytrends.csv";
     try:
         date_last_mod = datetime.date.fromtimestamp(os.path.getmtime(apple_hidden_path))
@@ -321,7 +353,7 @@ def _update_apple():
             os.system(powershell_path + "touch %s" % apple_hidden_path)
             return 0
         # attempt to find url:
-        for i in range(13, 13+10):
+        for i in range(14, 14+10):
             for dev in range(20):
                 if os.system("curl -f -o %s -z %s %s" % (apple_hidden_path, apple_hidden_path, _apple_web_path % (i, dev, attempt_date.strftime('%Y-%m-%d')))) == 0:
                     os.system(powershell_path + "cp %s %s" % (apple_hidden_path, _apple_local_path))
@@ -329,6 +361,12 @@ def _update_apple():
                     return 0
     print("Unable to update Apple mobility data", file=sys.stderr)
     return 1
+'''
+
+
+
+
+
 
 # update IHME data
 def _update_IHME():
@@ -382,8 +420,6 @@ def _construct_file_hierarchy():
     _create_dir("../data/temperature")
     
     _create_dir("../data/covid/CTP")
-
-
 
 
 
